a926b288b35089adfeac52a7998dafeaaf1d28fa
kiyari005 <letuankiet005@gmail.com>
2024-11-09 19:35:05 +0700

    Update index.js

--- WebApp/pages/index.js
+++ WebApp/pages/index.js
@@ -5,7 +5,6 @@ import LeftSidebar from "../components/LeftSidebar";
 import RightSidebar from "../components/RightSidebar";
 import Feed from "../components/Feed";
 import { getSession, useSession } from "next-auth/react";
-import PostList from "../components/Postlist";
 
 export default function Home() {
   const { data: session } = useSession();
