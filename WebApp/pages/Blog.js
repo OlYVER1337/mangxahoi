d1d2cda8a8c313179eb902a52bf0adf88a0dc808
kiyari005 <letuankiet005@gmail.com>
2024-12-02 17:31:20 +0700

    Blog

--- WebApp/pages/Blog.js
+++ WebApp/pages/Blog.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
-import Post from "../components/Post"; // Đảm bảo đường dẫn này là chính xác
+import Post from "../components/Post";
 import Navbar from "../components/Navbar";
+import RightSidebar from "../components/RightSidebar";
+import WhatsOnYourMind from "../components/WhatOnYourMind";
 import { collection, onSnapshot, query, orderBy, where } from "firebase/firestore";
 import { db } from "../firebase";
 
@@ -13,40 +15,44 @@ const Blog = () => {
     if (session) {
       const q = query(
         collection(db, "posts"),
-        where("userId", "==", session.user.id), // Lọc bài đăng theo userId
-        orderBy("postTimestamp", "desc")
+        where("userId", "==", session.user.id),
+        orderBy("postTimestamp", "desc") // Sắp xếp theo ngày đăng mới nhất
       );
       const unsubscribe = onSnapshot(q, (snapshot) => {
         setPosts(snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })));
       });
 
       return () => unsubscribe();
+    } else {
+      setPosts([]);
     }
   }, [session]);
 
   return (
     <main>
       <Navbar />
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-[600px]">
-        <div className="flex items-center mb-4">
-          <img
-            className="w-12 h-12 rounded-full"
-            src={session?.user?.image}
-            alt="dp"
-          />
-          <h1 className="ml-4 text-xl font-semibold">{session?.user?.name}</h1>
+      <RightSidebar />
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-[600px]">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center">
+              <img src={session?.user?.image} alt="dp" className="w-12 h-12 rounded-full" />
+              <h1 className="ml-4 text-xl font-semibold">{session?.user?.name}</h1>
+            </div>
+            <a href="EditProfile" className="bg-primary text-white py-2 px-4 rounded-lg mt-4">
+              Edit Profile
+            </a>
+          </div>
         </div>
-        <button className="bg-primary text-white py-2 px-4 rounded-lg mb-4">
-          Edit Profile
-        </button>
-        <div>
+        <div className="flex flex-col w-full max-w-[600px]">
+          <WhatsOnYourMind />
+        </div>
+        <div className="flex flex-col w-full max-w-[600px]">
           {posts.map((post) => (
-            <Post key={post.id} data={post} id={post.id} />
+            <Post key={post.id} data={post} id={post.id} className="mb-4" />
           ))}
         </div>
       </div>
-    </div>
     </main>
   );
 };
